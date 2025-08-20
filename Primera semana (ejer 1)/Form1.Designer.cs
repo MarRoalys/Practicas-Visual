6245d2db6bb4a1f2151a4b4eb4b4dfb14d1f70e2
Mariela Roa <roamariela905@gmail.com>
2025-08-20 13:48:06 -0600

    Add files via upload

---
+++ Primera semana (ejer 1)/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctUsuario = new System.Windows.Forms.TextBox();
+            this.ctClave = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.butIniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // ctUsuario
+            // 
+            this.ctUsuario.Location = new System.Drawing.Point(186, 81);
+            this.ctUsuario.Name = "ctUsuario";
+            this.ctUsuario.Size = new System.Drawing.Size(378, 22);
+            this.ctUsuario.TabIndex = 1;
+            // 
+            // ctClave
+            // 
+            this.ctClave.Location = new System.Drawing.Point(186, 148);
+            this.ctClave.Name = "ctClave";
+            this.ctClave.PasswordChar = '*';
+            this.ctClave.Size = new System.Drawing.Size(378, 22);
+            this.ctClave.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contrasena";
+            // 
+            // butIniciar
+            // 
+            this.butIniciar.Location = new System.Drawing.Point(282, 203);
+            this.butIniciar.Name = "butIniciar";
+            this.butIniciar.Size = new System.Drawing.Size(181, 35);
+            this.butIniciar.TabIndex = 4;
+            this.butIniciar.Text = "INICIAR SESION";
+            this.butIniciar.UseVisualStyleBackColor = true;
+            this.butIniciar.Click += new System.EventHandler(this.butIniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butIniciar);
+            this.Controls.Add(this.ctClave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ctUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ctUsuario;
+        private System.Windows.Forms.TextBox ctClave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button butIniciar;
+    }
+}
+
---
+++ Primera semana (ejer 1)/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        int intentos = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butIniciar_Click(object sender, EventArgs e)
+        {
+            string usuario, clave;
+            usuario = ctUsuario.Text;
+            clave = ctClave.Text;
+
+            if(usuario =="" || clave== "")
+            {
+                MessageBox.Show("Complete todos los campos.", "Error de inicio de sesion");
+            }
+            if (usuario == "admin" && clave == "admin123")
+            {
+                MessageBox.Show("Bienvenido al sistema "  + usuario);
+                ctUsuario.Clear();
+                ctClave.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("Usuario o clave incorrectos,numero de intentos restantes: " + (3-intentos),"Error de inicio de sesion", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                intentos= intentos+1;
+                if (intentos>3)
+                {
+                    MessageBox.Show("Has superado el numero de intentos", "Error de inicio de sesion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+            }
+        }
+    }
+}
